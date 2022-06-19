1d197acbfd8415ce6bb9c6b3ed3bb0eca027c27b
rzky141200 <rzky141200@gmail.com>
2022-06-19 11:32:42 +0700

    8 Introduction To Methods

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -10,15 +10,26 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintInsctruction();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInsctruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
         transform.Translate(xValue, 0, zValue);
-
     }
 }
